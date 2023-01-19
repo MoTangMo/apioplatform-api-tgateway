3059c573502848c0156eaf5853e37436d189f7e3
tangzhaofeng <www1532621280@qq.com>
2023-01-19 18:36:32 +0800

    初始化API网关

---
+++ apioplatform-api-tgateway/src/main/java/apiplatform/api/tgateway/apioplatformapitgateway/ApioplatformApiTgatewayApplication.java
@@ -0,0 +1,13 @@
+package apiplatform.api.tgateway.apioplatformapitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApioplatformApiTgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApioplatformApiTgatewayApplication.class, args);
+    }
+
+}
---
+++ apioplatform-api-tgateway/src/test/java/apiplatform/api/tgateway/apioplatformapitgateway/ApioplatformApiTgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package apiplatform.api.tgateway.apioplatformapitgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApioplatformApiTgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
